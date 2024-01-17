fd9270f1628e67fdf839bb2604a5fe464e8695a0
RenzoSC <76189720+RenzoSC@users.noreply.github.com>
2024-01-17 20:17:25 -0300

    setting up react

--- backend/shop/views.py
+++ backend/shop/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializer import ProductSerializer
 from .models import Product
+
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def home(request):
     return render(request, "index.html")
 
+def signup(request):
+    return render(request, "signup.html",{
+        "form":
+    })
 class ProductView(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
