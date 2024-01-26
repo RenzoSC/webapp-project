623392695da0fd4e3b7c5a7e1f4c0d94950c796b
RenzoSC <76189720+RenzoSC@users.noreply.github.com>
2024-01-26 18:54:09 -0300

    navbar para desktop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import Footbar from "./Footbar";
-
+import NavBar from "./Navbar";
 function App() {
   return (
     <div className="flex flex-col h-screen">
+      <NavBar/>
       <main className="flex-grow"></main>
       <Footbar/>
     </div>
--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,11 +1,13 @@
 function NavBar(){
-    return (<nav>
-        <ul>
-            <li>Home</li>
-            <li>Products</li>
-            <li>Who we are?</li>
-            <li>Localization</li>
-            <li>Perfil</li>
+    return (<nav className="max-w-container bg-pink-400">
+        <ul className="md:grid grid-cols-5 py-6 text-white font-semibold">
+            <li className="text-center cursor-pointer">Inicio</li>
+            <li className="text-center cursor-pointer">Productos</li>
+            <li className="text-center cursor-pointer">Contacto</li>
+            {/* <li>Favoritos</li>
+            <li>Historial de compras</li> */}
+            <li className="text-center cursor-pointer">Carritoxd</li>
+            <li className="text-center cursor-pointer me-3">Cuenta</li>
         </ul>
     </nav>)
 }
