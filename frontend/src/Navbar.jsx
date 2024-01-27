0c47738d6bf60f2c388108b3a60bfb85a7516332
RenzoSC <76189720+RenzoSC@users.noreply.github.com>
2024-01-27 01:05:35 -0300

    Update Navbar.jsx

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,13 +1,13 @@
 function NavBar(){
     return (<nav className="max-w-container bg-pink-400">
-        <ul className="md:grid grid-cols-5 py-6 text-white font-semibold">
-            <li className="text-center cursor-pointer">Inicio</li>
-            <li className="text-center cursor-pointer">Productos</li>
-            <li className="text-center cursor-pointer">Contacto</li>
+        <ul className="md:grid grid-cols-8 text-white font-semibold">
+            <li className="text-center col-start-3 py-4 cursor-pointer hover:bg-pink-500">Inicio</li>
+            <li className="text-center py-4 cursor-pointer hover:bg-pink-500">Productos</li>
+            <li className="text-center py-4 cursor-pointer hover:bg-pink-500">Contacto</li>
             {/* <li>Favoritos</li>
             <li>Historial de compras</li> */}
-            <li className="text-center cursor-pointer">Carritoxd</li>
-            <li className="text-center cursor-pointer me-3">Cuenta</li>
+            <li className="text-center py-4 cursor-pointer hover:bg-pink-500">Carritoxd</li>
+            <li className="text-end col-start-8 cursor-pointer  flex justify-end"><span className="p-4 hover:bg-pink-500">Cuenta</span></li>
         </ul>
     </nav>)
 }
